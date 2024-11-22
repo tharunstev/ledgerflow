523c3d248f4249d20863b64d3d35a41c81ba2a05
tharunstev <163376561+tharunstev@users.noreply.github.com>
2024-11-22 17:46:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,15 +21,7 @@ app.use(bodyParser.json());
 app.use('/api',transactionRoute)
 app.use('/user',userRoute)
 app.get('/',(req,res)=>{
-    res.send("Welcome to LedgerFlow
-LedgerFlow is your go-to platform for seamless transaction management. Whether you're handling deposits, withdrawals, or simply keeping track of your financial activities, LedgerFlow provides an intuitive and efficient way to manage all your transactions.
-
-Key features of LedgerFlow include:
-
-Create Transactions: Effortlessly create new transactions, including deposits and withdrawals, specifying the amount, type, and associated user.
-Transaction History: Quickly access and view the transaction history for any user, with detailed information such as transaction type, amount, status, and timestamp.
-Update Transaction Status: Update the status of any transaction, marking it as Completed or Failed, to keep your records accurate and up-to-date.
-Detailed Transaction View: Get a comprehensive look at any individual transaction, including all relevant details such as transaction ID, amount, type, status, and timestamp.")
+    res.send("Welcome to LedgerFlow LedgerFlow is your go-to platform for seamless transaction management. Whether you're handling deposits, withdrawals, or simply keeping track of your financial activities, LedgerFlow provides an intuitive and efficient way to manage all your transactions.Key features of LedgerFlow include:Create Transactions: Effortlessly create new transactions, including deposits and withdrawals, specifying the amount, type, and associated user.Transaction History: Quickly access and view the transaction history for any user, with detailed information such as transaction type, amount, status, and timestamp.Update Transaction Status: Update the status of any transaction, marking it as Completed or Failed, to keep your records accurate and up-to-date.Detailed Transaction View: Get a comprehensive look at any individual transaction, including all relevant details such as transaction ID, amount, type, status, and timestamp.")
 })
 
 // --------------------------------------------------------------
