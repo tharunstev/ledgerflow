9812e899f02cde6312418aeeaa48120b926cf83f
tharunstev <163376561+tharunstev@users.noreply.github.com>
2024-11-23 13:44:24 +0530

    Update index.js
    Added home route and some data to it

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express=require("express")
 const app=express();
-const bodyParser=require('body-parser')
 const dotenv=require('dotenv')
 const mongoose=require('mongoose')
-const path=require('path');
+
 
 const transactionRoute=require('./routes/transaction.route')
 const userRoute=require('./routes/user.route')
@@ -11,17 +10,32 @@ const userRoute=require('./routes/user.route')
 const Port=5000;
 
 dotenv.config()
-app.set('view ejs','ejs');
-app.use(express.urlencoded({extended:true}));
-app.use(bodyParser.json());
+
 
 
 // ----------------------------ROUTES----------------------------------
 
 app.use('/api',transactionRoute)
 app.use('/user',userRoute)
+
 app.get('/',(req,res)=>{
-    res.send("Welcome to LedgerFlow LedgerFlow is your go-to platform for seamless transaction management. Whether you're handling deposits, withdrawals, or simply keeping track of your financial activities, LedgerFlow provides an intuitive and efficient way to manage all your transactions.Key features of LedgerFlow include:Create Transactions: Effortlessly create new transactions, including deposits and withdrawals, specifying the amount, type, and associated user.Transaction History: Quickly access and view the transaction history for any user, with detailed information such as transaction type, amount, status, and timestamp.Update Transaction Status: Update the status of any transaction, marking it as Completed or Failed, to keep your records accurate and up-to-date.Detailed Transaction View: Get a comprehensive look at any individual transaction, including all relevant details such as transaction ID, amount, type, status, and timestamp.")
+    res.send(`<html><body>
+        <h1>WELCOME TO LEDGERFLOW</h1>
+        <h2>Transaction Management </h2>
+        <h3>1. POST /api/transactions/ : this is to make a transaction </h3>
+          <p>userid-1 :673dca323c0e47d2f8cb2705 </p>
+        <p>userid-2 :673df08bf7b3073a1ff06fec </p>
+        <h3>2. GET /api/transactions/user_id : this is for all transactions of a user</h3>
+        <p>userid-1 :673dca323c0e47d2f8cb2705 </p>
+        <p>userid-2 :673df08bf7b3073a1ff06fec </p>
+        <h3>4. GET /api/transaction/transaction-id/  : this is for a particular transaction </h3>
+        <p>transactionid-1 : 673f161cc3ecfcdd245892f5</p>
+        <p>transactionid-2 : 6740382d8fe73b7e21b15a1a</p>
+        <h3>3. PUT /api/updatetransaction/transaction-id/ : to update status of a particular transaction </h3>
+       <p>transactionid-1 : 673f161cc3ecfcdd245892f5</p>
+        <p>transactionid-2 : 6740382d8fe73b7e21b15a1a</p>
+        </body></html>`)
+    
 })
 
 // --------------------------------------------------------------
